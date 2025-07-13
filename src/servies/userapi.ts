4f2e222f918d4352882945a4a391d9fa2b1c594e
Arunkumar-30 <Selvam.arun100@gmail.com>
2025-07-13 20:05:32 +0530

    ddw

--- src/servies/userapi.ts
+++ src/servies/userapi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://admin-panel-backend-prlz.onrender.com/api',
 });
 
 export const register = (data: any) => API.post('/register', data);
